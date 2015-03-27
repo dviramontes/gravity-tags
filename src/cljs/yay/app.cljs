28a678059a8ddfe6e5a78e6c8ea92e0fdd6f95b5
dviramontes <dviramontes@gmail.com>
2015-03-26 23:14:45 -0600

    fixes .ajax call

--- src/cljs/yay/app.cljs
+++ src/cljs/yay/app.cljs
@@ -3,26 +3,37 @@
 
 (enable-console-print!) ;; enable print at web inspector console
 
-(def url "http://feeds.delicious.com/v2/json/tags/dviramontes")
+(defonce url "http://feeds.delicious.com/v2/json/tags/dviramontes")
 
-(defn get-tags-from-delicious []
-  (-> (.ajax js/$ url (clj->js {:crossDomain true 
-                        :dataType "jsonp"})) 
-      .success (fn [x]) (println x)))
+(def tags (atom nil))
 
-(defn some-component []
-  [:div
-   [:h3 "I am a component!"]
-   [:p.someclass
-    "I have " [:strong "bold"]
-    [:span {:style {:color "red"}} " and red"]
-    " text."]])
+(defn get-tags []
+  (.done 
+   (.ajax js/$ url (clj->js {:crossDomain true 
+                             :dataType "jsonp"})) 
+   (fn [x] 
+     ;;(print (.stringify js/JSON x))
+     (reset! tags (js->clj x :keywordize-keys true)))))
 
-(defn calling-component []
+(defn child-component []  
+  (let []
+    (fn []
+      [:div.container
+       [:div.col-lg-12
+        [:h1 "imm.aterial.org"]
+        [:p.name "David Viramontes" 
+         [:span {:style {:color "pink"}} "@"]
+         [:p  "---"]]]])))
+
+(defn parent-component []
   [:div.container
-     [:div.row 
-      [:div.col-lg-12 [some-component]]]])
+   [:div.row 
+    [:div.col-lg-12 [child-component]]]])
+
+(defn react-component []
+  (reagent/create-class {:reagent-render parent-component
+                         :component-did-mount get-tags}))
 
 (defn init []
-  (reagent/render-component [calling-component]
+  (reagent/render-component [react-component]
                             (.getElementById js/document "mount")))
